2c04c24e46dd545c3774bdc82c33ed075e18efd5
Jamswhat2 <hilarionjames25@gmail.com>
2021-02-16 01:17:33 -0500

    Added FlixsterApp codes

---
+++ app/src/main/java/com/codepath/flixster/adapters/MovieAdapter.java
@@ -0,0 +1,85 @@
+package com.codepath.flixster.adapters;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.flixster.R;
+import com.codepath.flixster.models_.Movie;
+
+import java.util.List;
+
+public class MovieAdapter extends  RecyclerView.Adapter<MovieAdapter.Viewholder>{
+
+    Context context;
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    // Usually involves inflating a layout from XML and returning the holder..
+    @NonNull
+    @Override
+    public Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("MovieAdapter","onCreateViewHolder");
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+        return new Viewholder(movieView);
+    }
+
+    //Involves populating data into the item through holder.
+    @Override
+    public void onBindViewHolder(@NonNull Viewholder holder, int position) {
+        Log.d("MovieAdapter","onBindViewHolder" + position);
+        // Get the movie at the passedd in position
+        Movie movie = movies.get(position);
+        // Bind the movie data into the VH
+        holder.bind(movie);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class Viewholder extends RecyclerView.ViewHolder {
+
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public Viewholder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+        }
+
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+            String imageUrl;
+            // if phone in landscape -> imageUrl = backdrop image
+            // else imageUrl = poster image.
+
+            if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                imageUrl = movie.getBackdropPath();
+            } else {
+                imageUrl = movie.getPosterPath();
+            }
+
+            Glide.with(context).load(imageUrl).into(ivPoster);
+        }
+    }
+}   //OnBindViewholder more efficient than Oncreateviewholder (less created views)..
